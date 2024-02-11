84bfa3a9a9cf795a38cdaef012155981176c751a
Wagner Coche <wagnercoche@gmail.com>
2024-02-11 18:08:08 +0100

    🐳 chore: assets and app icon configured

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'src/core/presenter/app/app_widget.dart';
 import 'src/core/presenter/config/device_global_config.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   DeviceGlobalConfig.setVerticalOrientation();
   runApp(const AppWidget());
 }
